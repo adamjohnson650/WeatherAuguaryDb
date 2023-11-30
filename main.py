4875b0cdddeb8b19ec661fbe360467b2d6498c29
adamjohnson650 <133832291+adamjohnson650@users.noreply.github.com>
2023-11-29 20:02:04 -0800

    Add a chart and show current and future weather

--- main.py
+++ main.py
@@ -2,62 +2,30 @@
 #WeatherAuguaryDb
 #CNE340 Fall 2023
 
-import json
-import time
-import html2text
-import mysql.connector
+import pandas  as pd
 import requests
-from datetime import datetime
-import pandas as pd
+import matplotlib.pyplot as plt
+from sqlalchemy import create_engine
+from sqlalchemy import text
+hostname = "127.0.0.1"
+username = "root"
+pwd = ""
+dbname = "WeatherAuguaryDb"
 
-API_URL = 'https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/seattle%20WA?unitGroup=metric&key=KFH6UKWSHD64C8H57RYLREY4R&contentType=json'
-
-
-def connect_to_sql():
-    conn = mysql.connector.connect(user='root', password='',
-                                   host='127.0.0.1', database='WeatherAuguaryDb')
-    return conn
-
-def create_tables(cursor):
-    cursor.execute('''CREATE TABLE IF NOT EXISTS weather_data (
-            id INT PRIMARY KEY auto_increment,
-            date DATETIME,
-            temp Float,
-            humidity FLOAT,
-            precip FLOAT,
-            windspeed FLOAT,
-            conditions VARCHAR 500
-            );''')
-
-def query_sql(cursor, query):
-    cursor.execute(query)
-    return cursor
-def fetch_and_insert_weather_data():
-    conn = connect_to_sql()
-    cursor = conn.cursor()
-
-    # Fetch data from the API
-    response = requests.get(API_URL)
-
-    if response.status_code == 200:
-        weather_data = response.json()
-        return weather_data
-    else:
-        print('Failed to retrieve data:', response.status_code)
-        return None
-def process_data_and_insert(data):
-    if data:
-        days_data = data.get('days', [])
-        df = pd.DataFrame(days_data)
-        print(df.head())
-        conn_string = 'mysql+pymysql://root:@localhost/WeatherAuguaryDb'  # Adjust connection string as needed
-        engine = create_engine(conn_string)
-
-        df.to_sql('weather_data', con=engine, if_exists='append', index=False)
-    else:
-        print('No data to process.')
-
-    weather_data = fetch_weather_data()
-    process_data_and_insert(weather_data)
-
-    conn.close()
+engine = create_engine("mysql+pymysql://{user}:{pw}@{host}/{db}"
+.format(host=hostname, db=dbname, user=username, pw=pwd))
+api_url = 'https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/seattle%20WA?unitGroup=metric&key=KFH6UKWSHD64C8H57RYLREY4R&contentType=json'
+response = requests.get(api_url)
+if response.status_code == 200:
+    data = response.json()
+    weather_df = pd.DataFrame(data['days'])
+    weather_df['datetime'] = pd.to_datetime(weather_df['datetime'])
+plt.figure(figsize=(10, 6))
+plt.plot(weather_df['datetime'], weather_df['tempmax'], label='Max Temperature')
+plt.plot(weather_df['datetime'], weather_df['tempmin'], label='Min Temperature')
+plt.xlabel('Date')
+plt.ylabel('Temperature (°F)')
+plt.title('Weather Trends in Seattle')
+plt.legend()
+plt.grid(True)
+plt.show()
