6c8f843fc8ef955db31d68e646a868b0ca03486c
adamjohnson650 <133832291+adamjohnson650@users.noreply.github.com>
2023-12-01 13:45:47 -0800

    Create 3 graphs Plot Temp Plot Wind Speed Plot Precipitation

--- main.py
+++ main.py
@@ -20,12 +20,37 @@ if response.status_code == 200:
     data = response.json()
     weather_df = pd.DataFrame(data['days'])
     weather_df['datetime'] = pd.to_datetime(weather_df['datetime'])
-plt.figure(figsize=(10, 6))
-plt.plot(weather_df['datetime'], weather_df['tempmax'], label='Max Temperature')
-plt.plot(weather_df['datetime'], weather_df['tempmin'], label='Min Temperature')
-plt.xlabel('Date')
-plt.ylabel('Temperature (°F)')
-plt.title('Weather Trends in Seattle')
-plt.legend()
-plt.grid(True)
+
+
+weather_df['windspeedkm'] = weather_df['windspeed']*1.60934
+
+
+fig, (ax1, ax2, ax3) = plt.subplots(3, 1, figsize=(10, 15))
+
+# Plot temperature
+ax1.plot(weather_df['datetime'], weather_df['tempmax'], label='Max Temperature')
+ax1.plot(weather_df['datetime'], weather_df['tempmin'], label='Min Temperature')
+ax1.set_xlabel('Date')
+ax1.set_ylabel('Temperature (°F)')
+ax1.set_title('Weather Trends in Seattle - Temperature')
+ax1.legend()
+ax1.grid(True)
+
+# Plot wind speed
+ax2.plot(weather_df['datetime'], weather_df['windspeedkm'], color='green', label='Wind Speed (km/h)')
+ax2.set_xlabel('Date')
+ax2.set_ylabel('Wind Speed (km/h)')
+ax2.set_title('Weather Trends in Seattle - Wind Speed')
+ax2.legend()
+ax2.grid(True)
+
+# Plot precipitation
+ax3.bar(weather_df['datetime'], weather_df['precip'], color='blue', label='Precipitation')
+ax3.set_xlabel('Date')
+ax3.set_ylabel('Precipitation')
+ax3.set_title('Weather Trends in Seattle - Precipitation')
+ax3.legend()
+ax3.grid(True)
+
+plt.tight_layout()
 plt.show()
